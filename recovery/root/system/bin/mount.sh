a1da00a878bfae6c476505bdb61e3266939e8df4
Jamie Falconer <mrfluffyoven@gmail.com>
2025-01-05 15:40:38 +0000

    Update mount.sh
    Signed-off-by: Jamie Falconer <mrfluffyoven@gmail.com>

--- recovery/root/system/bin/mount.sh
+++ recovery/root/system/bin/mount.sh
@@ -1,10 +1,2 @@
 #!/system/bin/sh
-#
-# mount vendor on boot
-
-echo " - Mounting /vendor..."
-mount -o ro /vendor &>/dev/null
-if ! mount | grep -q /vendor; then
-  echo " -   Mount failed. Aborting..."
-  exit 1
-fi
+mount -o ro /vendor
